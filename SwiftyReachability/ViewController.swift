2f411d0c538b17ac3fce89609b79d41411b4d061
Sugam Kalra <sugamkalra@Sugams-MacBook-Pro.local>
2015-12-08 17:45:44 +0530

    Swift Reachability

--- SwiftyReachability/ViewController.swift
+++ SwiftyReachability/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btnCheckNetworkPressed(sender: UIButton)
+    {
+        
+        // Check for network first
+        
+        if !Reachability.isConnectedToNetwork()
+        {
+            let alertController: UIAlertController = UIAlertController(title: "No Network Available", message: "Please Check Your Internet Connection", preferredStyle: .Alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style:.Default) { (action) -> Void in
+                
+            }
+            
+            alertController.addAction(okAction)
+            
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+        
+        }
+            
+        else
+        {
+            let alertController: UIAlertController = UIAlertController(title: "Network Available", message: "Internet Connection is Available", preferredStyle: .Alert)
+            
+            let okAction = UIAlertAction(title: "Ok", style:.Default) { (action) -> Void in
+                
+            }
+            
+            alertController.addAction(okAction)
+            
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
